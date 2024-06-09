7e9f39ad6ba5333ba7130c81e9ac2930b11cf597
sachin <sachin.marnur99@gmail.com>
2024-06-09 18:24:58 +0530

    First Commit

---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,16 @@
+import CallList from '@/components/CallList'
+import React from 'react'
+
+const Recordings = () => {
+  return (
+    <section className='flex flex-col size-full gap-10 text-white'>
+      <h1 className='text-3xl font-bold'>
+        Recordings
+      </h1>
+
+      <CallList type='recordings' />
+    </section>
+  )
+}
+
+export default Recordings
